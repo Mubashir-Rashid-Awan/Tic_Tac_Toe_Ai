2747854c455c914cf8711ec9753ba616bc0633ad
Mubashir Rashid <mubashirrashid1031@gmail.com>
2026-05-21 16:56:16 +0500

    Smart Ai Moves

--- main.py
+++ main.py
@@ -75,13 +75,40 @@ def Random_ai_move():
         row, col = empty_cells[np.random.choice(len(empty_cells))]
         board[row][col] = 2
 
+def smart_ai_move():
+    empty_cells = np.argwhere(board == 0)   
+    for row,col in empty_cells:         # check if AI can win in the next move
+        board[row][col] = 2
+        if check_win(2):
+            return
+        board[row][col] = 0
+
+    for row,col in empty_cells:         # check if player can win in the next move and block it
+        board[row][col] = 1
+        if check_win(1):
+            board[row][col] = 2
+            return
+        board[row][col] = 0    
+
+    if board[1][1] == 0:                 # take center if available take it
+        board[1][1] = 2
+        return
+    
+    corners = [(0, 0), (0, 2), (2, 0), (2, 2)]
+    for row, col in corners:             # take any available corner
+        if board[row][col] == 0:
+            board[row][col] = 2
+            return
+    
+    Random_ai_move()                      # if no winning move, blocking move, center or corner is available, take a random move
+
 
 current_player = 1
 while True:
     if current_player == 1:
         player_mov(current_player)
     else:
-        Random_ai_move()
+        smart_ai_move()
     os.system("cls")
     printboard()
     if check_win(current_player):
